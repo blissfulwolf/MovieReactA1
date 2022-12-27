10f7bdce088bf5a7fd5a2d22b910f8b1b94e1456
Jasper Teo <cashgrid@yahoo.com>
2022-12-27 13:02:21 +0800

    movie form created

--- movie-react-a1/src/App.js
+++ movie-react-a1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MovieForm from './components/MovieForm';
 
 function App() {
   return (
     <div className="movie-app">
-      <h1>Movie Assignment</h1>
+      <h1>Movie Assignment 1</h1>
+      <MovieForm />
     </div>
   );
 }
--- movie-react-a1/src/components/MovieForm.js
+++ movie-react-a1/src/components/MovieForm.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 
-function MovieForm() {
-  const [input, setInput] = useState("");
+function MovieForm(props) {
+    const [input, setInput] = useState("");
 
-  return (
-    <form className="movie-form">
-      <input
-        type="text"
-        placeholder="Add a movie"
-        value={input}
-        name="text"
-        className="movie-input"
-      />
-      <button className="movie-button">Add Movie</button>
-    </form>
-  );
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+
+    // handleSubmit to prevent the brower from 
+    // refreshing when clicking the submit button
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // props.onSubmit({
+        //     text: input
+        // })
+
+        setInput("");
+    };
+
+    return (
+        <form className="movie-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Add a movie"
+                value={input}
+                name="text"
+                className="movie-input"
+                onChange={handleChange}
+            />
+            <button className="movie-button">Add Movie</button>
+        </form>
+    );
 }
 
 export default MovieForm;
